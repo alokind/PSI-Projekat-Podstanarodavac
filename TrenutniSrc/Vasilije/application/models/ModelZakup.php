205c78016536681b2a0660adfdfd70578d1f3892
Bojana Krivokapic <48254026+bojanakri@users.noreply.github.com>
2019-05-31 02:16:59 +0200

    Create ModelZakup.php

---
+++ TrenutniSrc/Vasilije/application/models/ModelZakup.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of ModelZakup
+ *
+ * @author bojanakri
+ */
+class ModelZakup extends CI_Model{
+    public function __construct() {
+        parent::__construct();
+    }
+}
