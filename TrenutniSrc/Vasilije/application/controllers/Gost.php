9ca4cff420d1a610b23848ac6ce902658fd5af52
Bojana Krivokapic <48254026+bojanakri@users.noreply.github.com>
2019-05-31 02:06:09 +0200

    tip P je podstanar

--- TrenutniSrc/Vasilije/application/controllers/Gost.php
+++ TrenutniSrc/Vasilije/application/controllers/Gost.php
@@ -77,7 +77,7 @@ class Gost extends CI_Controller{
                 $this->session->set_userdata('korisnik', $korisnik);
                 
                 //U zavisnosti od tipa korisnika, odlazi se na odgovarajuci kontroler
-                if ($korisnik->Tip == 'S') {
+                if ($korisnik->Tip == 'P') {
                     redirect("Podstanar");
                 } else {
                     redirect("Stanodavac");
